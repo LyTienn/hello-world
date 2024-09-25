00d0b510faaacee64f7640e276b5e499d662a0ae
Ly Cong Tien <134036669+LyTienn@users.noreply.github.com>
2024-09-25 17:09:32 +0700

    Add files via upload

---
+++ Lec1LTHDT.java
@@ -0,0 +1,17 @@
+
+package Lec1LTHDT;
+
+/**
+ *
+ * @author ASUS
+ */
+import javax.swing.JOptionPane;
+public class Lec1LTHDT {
+    public static void main(String[] args) {
+        String result;
+        result = JOptionPane.showInputDialog("Hay nhap ten ban:");
+        JOptionPane.showMessageDialog(null,"Xin chao " + result + " !");
+        System.exit(0);
+    }
+    
+}
